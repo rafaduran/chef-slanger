131776e86c98ef226d4378363359677ce91ca5cc
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2012-09-22 20:09:26 +0200

    Adding cookbook information.
    Filled in README.md and metadata.rb

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
 Description
 ===========
+Installs Slanger using rvm and runs it as upstar job.
+
+This cookbook requires [rvm cookbook](https://github.com/fnichol/chef-rvm) and,
+since it requires a running Redis server to work properly, you probably also
+want to install a Redis cookbook, e.g. [redisio](http://cookbooks.opscode.com/api/v1/cookbooks/redisio).
 
 Requirements
 ============
+This cookbook only supports Ubuntu systems, thought it should work on any of
+the rvm cookbook supported OS.
 
 Attributes
 ==========
+The file attributes/default.rb contains the following attribute types:
+
+ * Upstart job specific attributes.
+ * Slanger specific attributes, see [Slanger at GitHub](https://github.com/stevegraham/slanger)
+   for further information.
 
 Usage
 =====
+You can include default recipe from your coobooks:
 
+    include_recipe 'slanger'
+
+You can also just add this cookbook to your run list.
--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,30 @@ license          "Apache 2.0"
 description      "Installs/Configures slanger"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+name             'slanger'
+supports         'ubuntu'
+
+recipe  'slanger',
+        'Installs and configures Slager as upstart job'
+
+# rvm cookbook comes from
+# https://github.com/fnichol/chef-rvm
+depends 'rvm'
+
+attribute 'slanger/app_key',
+  :display_name => 'application key',
+  :description  => 'This is the Pusher app key you want to use',
+  :default      => 'appkey',
+  :required     => 'recommended'
+
+attribute 'slanger/secret',
+  :display_name => 'secret key',
+  :description  => 'This is your Pusher secret',
+  :default      => 'secret',
+  :required     => 'recommended'
+
+attribute 'slanger/respawn',
+  :display_name => 'upstart respawn',
+  :description  => 'Upstart respawn stanza boolean flag',
+  :default      => 'true',
+  :required     => true
